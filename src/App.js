eab820ad2ff89f540a0434ad590c062df65a18a9
tim_coder <itteamdev74@gmail.com>
2022-10-19 07:24:10 +0500

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { storage } from "./firebase";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const contextEvent = (e) => e.preventDefault();
+  const [progress, setProgress] = useState(0);
+  const [link, setLink] = useState("");
+  const formHandler = (e) => {
+    e.preventDefault();
+    const file = e.target[0].files[0];
+    uploadFiles(file);
+  };
+
+  const uploadFiles = (file) => {
+    if (!file) return;
+    const storageRef = ref(storage, `/files/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const prog = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(prog);
+      },
+      (err) => console.log(err),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => setLink(url));
+      }
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-3">
+      <div className="title mb-3">
+        <h1>Firebase file upload!</h1>
+        <h2>How to upload files to firebase ?</h2>
+      </div>
+
+      <div className="row">
+        <div className="col-md-6 d-flex align-items-center justify-content-center flex-column">
+          <form onSubmit={formHandler} className="d-flex">
+            <input type="file" className="form-control" />
+            <button className="btn btn-success ms-2 d-flex align-items-center">
+              Submit <i className="fa-solid fa-cloud-arrow-up ms-2"></i>
+            </button>
+          </form>
+        </div>
+        <div className="col-md-6">
+          <div className="preview d-flex align-items-center justify-content-center flex-column">
+            <h2>Uploaded {progress} % </h2>
+            {link ? (
+              <img
+                onContextMenu={contextEvent}
+                alt="Uploaded file"
+                src={link}
+                className="img-fluid"
+              />
+            ) : (
+              <p>Please Upload image</p>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
